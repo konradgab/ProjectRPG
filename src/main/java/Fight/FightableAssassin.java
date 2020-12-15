ea72a8924c722790bf25260d99dfdc1b531cd38a
Konrad Gabrukiewicz <konrad.gabrukiewicz@gmail.com>
2020-12-15 18:31:21 +0100

    Add Board class

--- src/main/java/Fight/FightableAssassin.java
+++ src/main/java/Fight/FightableAssassin.java
@@ -2,6 +2,6 @@ package Fight;
 
 import java.util.List;
 
-public interface FightableAssasin extends Fightable {
+public interface FightableAssasin extends FightableEnemy {
     public List<String> getDialoguesList();
 }
---
+++ src/main/java/Fight/FightableEnemy.java
@@ -0,0 +1,2 @@
+package Fight;public interface FightAbleEnemy {
+}
